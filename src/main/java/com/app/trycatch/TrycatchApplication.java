c7ea14fed79d58704a0ae2a8320f3063b4efcdf3
fullypigchan <pigchan0202@gmail.com>
2026-02-04 13:45:05 +0900

    init

---
+++ src/main/java/com/app/trycatch/TrycatchApplication.java
@@ -0,0 +1,13 @@
+package com.app.trycatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrycatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrycatchApplication.class, args);
+    }
+
+}
